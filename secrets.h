261d54a6607e9aae9a1aa982cb72997fa44ef83a
ukkokalevala <148769692+ukkokalevala@users.noreply.github.com>
2025-05-03 14:18:34 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,7 +1,7 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 // NH74CL-F9XL2D-EU42SJ-5CZB
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
 
 #endif // SECRETS_H
